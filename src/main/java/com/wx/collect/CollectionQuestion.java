85dddeb3e1f46620b56def6a3b011890a2779914
weishengwx <2234346829@qq.com>
2022-12-05 20:26:07 +0800

    写了部分理论基础面试题

---
+++ src/main/java/com/wx/collect/CollectionQuestion.java
@@ -0,0 +1,13 @@
+package com.wx.collect;
+
+/**
+ * @author 22343
+ * @version 1.0
+ * List (对付顺序的好帮⼿)： 存储的元素是有序的、可重复的。
+ * Set (注重独⼀⽆⼆的性质): 存储的元素是⽆序的、不可重复的。
+ * Map (⽤ Key 来搜索的专家): 使⽤键值对（kye-value）存储，类似于数学上的函数
+ * y=f(x)，“x”代表 key，"y"代表 value，Key 是⽆序的、不可重复的，value 是⽆序的、可重复
+ * 的，每个键最多映射到⼀个值。
+ */
+public class CollectionQuestion {
+}
---
+++ src/main/java/com/wx/collect/ListQuestion.java
@@ -0,0 +1,19 @@
+package com.wx.collect;
+
+import org.junit.Test;
+
+/**
+ * @author 22343
+ * @version 1.0
+ */
+public class ListQuestion {
+	
+	/**
+	 *
+	 */
+	@Test
+	public void arrayListAndLinkedList(){
+	
+	}
+	
+}
